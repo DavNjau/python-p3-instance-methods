74d9da1fe4a524f8f105b29b928f6b5d1a259162
David <nnjui13@gmail.com>
2025-11-16 14:18:22 +0300

    Initial commit

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,15 @@
 
 class Person:
     # Class body goes here
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person id walking")
 
     #Instance method definition
     pass
+
+henry = Person()
+print(henry.talk())
+print(henry.walk())
